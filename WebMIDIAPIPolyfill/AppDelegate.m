d5ce045c52a50e8510e65aceb652af393edffa36
Xazu <Xazu001@users.noreply.github.com>
2024-01-18 09:45:57 +0100

    test

--- WebMIDIAPIPolyfill/AppDelegate.m
+++ WebMIDIAPIPolyfill/AppDelegate.m
@@ -25,6 +25,7 @@
     // Override point for customization after application launch.
 [[UINavigationBar appearance] setHidden:YES];
 [[UIToolBar appearance] setHidden:YES];
+ [navigationController setNavigationBarHidden:YES animated:NO];
 
     return YES;
 }
